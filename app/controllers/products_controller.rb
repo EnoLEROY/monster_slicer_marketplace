86a8e30859c50ec3f52b493c79cb9b8413db96c3
Arnaud Toulorge <a.toulorge1997@gmail.com>
2023-05-29 14:33:01 +0200

    Finalisation de user (ajout nom prenom dans la vue et autorisation dans le controller) + trame index products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_products, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
   end
