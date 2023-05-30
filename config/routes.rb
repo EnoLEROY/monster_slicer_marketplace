60136a6f63fac8c1f29738c37a4f2765fad76db2
Eliott Regnier <eliott2610@gmail.com>
2023-05-30 17:46:48 +0200

    added the renting form in product show with flatpickr

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :products, only: [:index, :show, :new, :create]
+  resources :products, only: [:index, :show, :new, :create] do
+    resources :rentings, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
