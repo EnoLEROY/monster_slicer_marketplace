b932b9aa435c92e20b56c010d6ab268d9b144639
Eno LEROY <eno.leroy@gmail.com>
2023-05-30 12:37:05 +0200

    creation du model renting avec les validations et les has_many belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :rentings
+
+  validates :title, :price, :description, :category, presence: true
+  # ligne a verifier pour recupérer les users qui louent un produit
+  # has_many :users, through: :rentings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_132933) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_101915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_132933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "rentings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "status", default: "pending"
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_rentings_on_product_id"
+    t.index ["user_id"], name: "index_rentings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_132933) do
   end
 
   add_foreign_key "products", "users"
+  add_foreign_key "rentings", "products"
+  add_foreign_key "rentings", "users"
 end
