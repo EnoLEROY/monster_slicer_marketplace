bb446dc85081c529f6b0e1af8bcfd95e62eeed79
Eliott Regnier <eliott2610@gmail.com>
2023-05-31 12:27:53 +0200

    small css updates

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -4,11 +4,15 @@ import Typed from "typed.js"; // You need to import this to use new Typed()
 
 export default class extends Controller {
   static values = {
-    sentences: Array
   }
 
   connect() {
-    const options = {strings: this.sentencesValue, typeSpeed: 40};
-    new Typed(this.element, options)
+    new Typed(this.element, {
+
+      strings: ['Welcome travelers ^1000 to the </br> Monster Slicer™ marketplace'],
+      typeSpeed: 40,
+      showCursor:false,
+      smartBackspace: true
+    });
   }
 }
